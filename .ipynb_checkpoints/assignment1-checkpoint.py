04f2995a6e2c3e057d0e62ffc94cde07c74ddb9c
Panagiotis Vardanis <panagiotisvardanis@gmail.com>
2019-06-08 18:06:39 +0200

    first

--- .ipynb_checkpoints/assignment1-checkpoint.py
+++ .ipynb_checkpoints/assignment1-checkpoint.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 N = 512
-min_value_db = -100.0 # min value in dB
-min_value = 10 ** (min_value_db / float(20))
+min_value = -100.0 # min value in dB
 max_value = 96.0 # max value in dB
 
 def scaled_fft_db(x):
@@ -22,9 +21,9 @@ def scaled_fft_db(x):
     # Your code goes here
     window = compute_hann()
     y = window * x
-    y_fft = np.fft.fft(y) / float(N) # the output of the fft normalized
+    y_fft = np.fft.fft(y) / N # the output of the fft normalized
     y_fft = np.abs(y_fft[:(N / 2 + 1)]) # take only the first half of the absolute values
-    y_fft = [20 * np.log10(x) if x >= min_value else min_value_db for x in y_fft]
+    y_fft = [20 * np.log10(i) if 20 * np.log10(i) >= min_value else min_value for i in y_fft]
     m = np.max(y_fft)
     m = m - max_value
     y_fft = y_fft - m
@@ -34,7 +33,7 @@ def scaled_fft_db(x):
 def compute_hann():
     w = np.zeros(N) # initialize window
     n = np.arange(len(w)) 
-    w = 0.5 * (1 - np.cos(2 * np.pi * n / (N-1)))
+    w = 0.5 * (1 - np.cos(2 * np.pi * n) / (N-1))
     c = np.sqrt((N-1) / np.sum(w ** 2))
     w = c * w
     
